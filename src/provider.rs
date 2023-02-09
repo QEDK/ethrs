2f7416ee3e208a7d2cb26bcc9de0190a10e0e76f
qedk <1994constant@gmail.com>
2023-02-09 04:26:53 +0400

    feat: add transaction count endpoint

--- src/provider.rs
+++ src/provider.rs
@@ -20,6 +20,16 @@ pub enum DefaultBlockParam {
     PENDING,
 }
 
+#[derive(Deserialize, Debug)]
+struct RPCResponse {
+    error: Option<String>,
+    result: Option<String>,
+}
+
+lazy_static! {
+    static ref ADDRESS_REGEX: Regex = Regex::new(r"0x[0-9A-Fa-f]{0,40}").unwrap();
+}
+
 impl Provider {
     pub fn new(_url: &str) -> Provider {
         let mut headers: HeaderMap = HeaderMap::new();
@@ -32,16 +42,28 @@ impl Provider {
     }
 
     pub fn block_number(&self) -> Result<u128, Box<dyn Error>> {
-        #[derive(Deserialize, Debug)]
-        struct EthBlockNumber {
-            error: Option<String>,
-            result: Option<String>,
-        }
-
-        let json: EthBlockNumber = self
+        let json: RPCResponse = self
             .client
             .post(&self.url)
             .body("{\"method\":\"eth_blockNumber\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}")
+            .headers(self.headers.clone())
+            .send()?
+            .json()?;
+
+        match json.error {
+            Some(err) => Err(err.into()),
+            None => Ok(u128::from_str_radix(
+                json.result.unwrap().strip_prefix("0x").unwrap(),
+                16,
+            )?),
+        }
+    }
+
+    pub fn gas_price(&self) -> Result<u128, Box<dyn Error>> {
+        let json: RPCResponse = self
+            .client
+            .post(&self.url)
+            .body("{\"method\":\"eth_gasPrice\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}")
             .headers(self.headers.clone())
             .send()?
             .json()?;
@@ -61,16 +83,6 @@ impl Provider {
         block_param: Option<DefaultBlockParam>,
         block_number: Option<u128>,
     ) -> Result<u128, Box<dyn Error>> {
-        #[derive(Deserialize, Debug)]
-        struct EthGetBalance {
-            error: Option<String>,
-            result: Option<String>,
-        }
-
-        lazy_static! {
-            static ref ADDRESS_REGEX: Regex = Regex::new(r"0x[0-9A-Fa-f]{0,40}").unwrap();
-        }
-
         match ADDRESS_REGEX.is_match(address) {
             true => {
                 let mut payload = String::new();
@@ -89,7 +101,92 @@ impl Provider {
 
                 payload.push_str("\"],\"id\":1,\"jsonrpc\":\"2.0\"}");
 
-                let json: EthGetBalance = self
+                let json: RPCResponse = self
+                    .client
+                    .post(&self.url)
+                    .body(payload.clone())
+                    .headers(self.headers.clone())
+                    .send()?
+                    .json()?;
+
+                match json.error {
+                    Some(err) => Err(err.into()),
+                    None => Ok(u128::from_str_radix(
+                        json.result.unwrap().strip_prefix("0x").unwrap(),
+                        16,
+                    )?),
+                }
+            }
+            false => Err("Invalid address".into()),
+        }
+    }
+
+    pub fn get_code(
+        &self,
+        address: &str,
+        block_param: Option<DefaultBlockParam>,
+        block_number: Option<u128>,
+    ) -> Result<String, Box<dyn Error>> {
+        match ADDRESS_REGEX.is_match(address) {
+            true => {
+                let mut payload = String::new();
+                payload.push_str("{\"method\":\"eth_getCode\",\"params\":[\"");
+                payload.push_str(address);
+                payload.push_str("\",\"");
+                match block_param {
+                    Some(DefaultBlockParam::EARLIEST) => payload.push_str("earliest"),
+                    Some(DefaultBlockParam::LATEST) => payload.push_str("latest"),
+                    Some(DefaultBlockParam::PENDING) => payload.push_str("pending"),
+                    None => match block_number {
+                        Some(block) => payload.push_str(&format!("0x{:x}", block)),
+                        None => payload.push_str("latest"),
+                    },
+                }
+
+                payload.push_str("\"],\"id\":1,\"jsonrpc\":\"2.0\"}");
+
+                let json: RPCResponse = self
+                    .client
+                    .post(&self.url)
+                    .body(payload.clone())
+                    .headers(self.headers.clone())
+                    .send()?
+                    .json()?;
+
+                match json.error {
+                    Some(err) => Err(err.into()),
+                    None => Ok(json.result.unwrap()),
+                }
+            }
+            false => Err("Invalid address".into()),
+        }
+    }
+
+    pub fn get_transaction_count(
+        &self,
+        address: &str,
+        block_param: Option<DefaultBlockParam>,
+        block_number: Option<u128>,
+    ) -> Result<u128, Box<dyn Error>> {
+        match ADDRESS_REGEX.is_match(address) {
+            true => {
+                let mut payload = String::new();
+                payload.push_str("{\"method\":\"eth_getTransactionCount\",\"params\":[\"");
+                payload.push_str(address);
+                payload.push_str("\",\"");
+                match block_param {
+                    Some(DefaultBlockParam::EARLIEST) => payload.push_str("earliest"),
+                    Some(DefaultBlockParam::LATEST) => payload.push_str("latest"),
+                    Some(DefaultBlockParam::PENDING) => payload.push_str("pending"),
+                    None => match block_number {
+                        Some(block) => payload.push_str(&format!("0x{:x}", block)),
+                        None => payload.push_str("latest"),
+                    },
+                }
+
+                payload.push_str("\"],\"id\":1,\"jsonrpc\":\"2.0\"}");
+
+                let json: RPCResponse = self
                     .client
                     .post(&self.url)
                     .body(payload.clone())
--- tests/provider.rs
+++ tests/provider.rs
@@ -1,47 +1,171 @@
 use ethrs::provider::DefaultBlockParam;
 use ethrs::provider::Provider;
+use lazy_static::lazy_static;
 use std::error::Error;
+
+lazy_static! {
+    static ref PROVIDER: Provider = Provider::new("https://rpc.sepolia.org");
+}
 
 #[test]
 fn test_block_number() -> Result<(), Box<dyn Error>> {
-    let provider: Provider = Provider::new("https://rpc.sepolia.org");
-    provider.block_number().unwrap();
+    assert!(PROVIDER.block_number().unwrap() > 2851600);
+    Ok(())
+}
+
+#[test]
+fn test_gas_price() -> Result<(), Box<dyn Error>> {
+    assert!(PROVIDER.gas_price().unwrap() >= 8);
+    Ok(())
+}
+
+#[test]
+fn test_get_transaction_count() -> Result<(), Box<dyn Error>> {
+    assert!(
+        PROVIDER
+            .get_transaction_count("0x0000000000000000000000000000000000000000", None, None)
+            .unwrap()
+            == 0
+    );
+    assert!(
+        PROVIDER
+            .get_transaction_count(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::EARLIEST),
+                None
+            )
+            .unwrap()
+            == 0
+    );
+    assert!(
+        PROVIDER
+            .get_transaction_count(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::LATEST),
+                None
+            )
+            .unwrap()
+            == 0
+    );
+    assert!(
+        PROVIDER
+            .get_transaction_count(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::PENDING),
+                None
+            )
+            .unwrap()
+            == 0
+    );
+    assert!(
+        PROVIDER
+            .get_transaction_count(
+                "0x0000000000000000000000000000000000000000",
+                None,
+                Some(PROVIDER.block_number().unwrap() - 1)
+            )
+            .unwrap()
+            == 0
+    );
     Ok(())
 }
 
 #[test]
 fn test_get_balance() -> Result<(), Box<dyn Error>> {
-    let provider: Provider = Provider::new("https://rpc.sepolia.org");
-    provider
-        .get_balance("0x0000000000000000000000000000000000000000", None, None)
-        .unwrap();
-    provider
+    assert!(
+        PROVIDER
+            .get_balance("0x0000000000000000000000000000000000000000", None, None)
+            .unwrap()
+            > 0
+    );
+    PROVIDER
         .get_balance(
             "0x0000000000000000000000000000000000000000",
             Some(DefaultBlockParam::EARLIEST),
             None,
         )
         .unwrap();
-    provider
-        .get_balance(
-            "0x0000000000000000000000000000000000000000",
-            Some(DefaultBlockParam::LATEST),
-            None,
-        )
-        .unwrap();
-    provider
-        .get_balance(
-            "0x0000000000000000000000000000000000000000",
-            Some(DefaultBlockParam::PENDING),
-            None,
-        )
-        .unwrap();
-    provider
-        .get_balance(
-            "0x0000000000000000000000000000000000000000",
-            None,
-            Some(provider.block_number().unwrap() - 1),
-        )
-        .unwrap();
+    assert!(
+        PROVIDER
+            .get_balance(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::LATEST),
+                None,
+            )
+            .unwrap()
+            > 0
+    );
+    assert!(
+        PROVIDER
+            .get_balance(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::PENDING),
+                None,
+            )
+            .unwrap()
+            > 0
+    );
+    assert!(
+        PROVIDER
+            .get_balance(
+                "0x0000000000000000000000000000000000000000",
+                None,
+                Some(PROVIDER.block_number().unwrap() - 1),
+            )
+            .unwrap()
+            > 0
+    );
+    Ok(())
+}
+
+#[test]
+fn test_get_code() -> Result<(), Box<dyn Error>> {
+    assert_eq!(
+        PROVIDER
+            .get_code("0x0000000000000000000000000000000000000000", None, None)
+            .unwrap(),
+        "0x".to_owned()
+    );
+    assert_eq!(
+        PROVIDER
+            .get_code(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::EARLIEST),
+                None,
+            )
+            .unwrap(),
+        "0x".to_owned()
+    );
+    assert_eq!(
+        PROVIDER
+            .get_code(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::LATEST),
+                None,
+            )
+            .unwrap(),
+        "0x".to_owned()
+    );
+    assert_eq!(
+        PROVIDER
+            .get_code(
+                "0x0000000000000000000000000000000000000000",
+                Some(DefaultBlockParam::PENDING),
+                None,
+            )
+            .unwrap(),
+        "0x".to_owned()
+    );
+    assert_eq!(
+        PROVIDER
+            .get_code(
+                "0x0000000000000000000000000000000000000000",
+                None,
+                Some(PROVIDER.block_number().unwrap() - 1),
+            )
+            .unwrap(),
+        "0x".to_owned()
+    );
+    assert_eq!(PROVIDER.get_code("0x790830c1eaab862fd35dbce2e7ea1aebce32fce3", None, None).unwrap(), "0x6060604052600436106100ae5763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166306fdde0381146100b8578063095ea7b31461014257806318160ddd1461017857806323b872dd1461019d5780632e1a7d4d146101c5578063313ce567146101db57806370a082311461020457806395d89b4114610223578063a9059cbb14610236578063d0e30db0146100ae578063dd62ed3e14610258575b6100b661027d565b005b34156100c357600080fd5b6100cb6102d3565b60405160208082528190810183818151815260200191508051906020019080838360005b838110156101075780820151838201526020016100ef565b50505050905090810190601f1680156101345780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b341561014d57600080fd5b610164600160a060020a0360043516602435610371565b604051901515815260200160405180910390f35b341561018357600080fd5b61018b6103dd565b60405190815260200160405180910390f35b34156101a857600080fd5b610164600160a060020a03600435811690602435166044356103eb565b34156101d057600080fd5b6100b6600435610531565b34156101e657600080fd5b6101ee6105df565b60405160ff909116815260200160405180910390f35b341561020f57600080fd5b61018b600160a060020a03600435166105e8565b341561022e57600080fd5b6100cb6105fa565b341561024157600080fd5b610164600160a060020a0360043516602435610665565b341561026357600080fd5b61018b600160a060020a0360043581169060243516610679565b600160a060020a033316600081815260036020526040908190208054349081019091557fe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c915190815260200160405180910390a2565b60008054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103695780601f1061033e57610100808354040283529160200191610369565b820191906000526020600020905b81548152906001019060200180831161034c57829003601f168201915b505050505081565b600160a060020a03338116600081815260046020908152604080832094871680845294909152808220859055909291907f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b9259085905190815260200160405180910390a350600192915050565b600160a060020a0330163190565b600160a060020a0383166000908152600360205260408120548290101561041157600080fd5b33600160a060020a031684600160a060020a03161415801561045b5750600160a060020a038085166000908152600460209081526040808320339094168352929052205460001914155b156104c257600160a060020a03808516600090815260046020908152604080832033909416835292905220548290101561049457600080fd5b600160a060020a03808516600090815260046020908152604080832033909416835292905220805483900390555b600160a060020a038085166000818152600360205260408082208054879003905592861680825290839020805486019055917fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9085905190815260200160405180910390a35060019392505050565b600160a060020a0333166000908152600360205260409020548190101561055757600080fd5b600160a060020a033316600081815260036020526040908190208054849003905582156108fc0290839051600060405180830381858888f19350505050151561059f57600080fd5b33600160a060020a03167f7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b658260405190815260200160405180910390a250565b60025460ff1681565b60036020526000908152604090205481565b60018054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103695780601f1061033e57610100808354040283529160200191610369565b60006106723384846103eb565b9392505050565b6004602090815260009283526040808420909152908252902054815600a165627a7a72305820976c9c45a8c1e47424c3304cee5b065aefb0c6539e9fb6b31dc3eee2abf17f650029");
     Ok(())
 }
